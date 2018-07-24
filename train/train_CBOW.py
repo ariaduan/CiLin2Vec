e6d61007240354b83c49b2ba18e42babdbdc4e28
Yuguang Duan <ariaduan@pku.edu.cn>
2018-07-24 10:34:55 +0800

    Add files via upload

---
+++ train/train_CBOW.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+import logging
+import os.path
+import sys
+import gensim
+import multiprocessing
+from gensim.corpora import WikiCorpus
+from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
+inp = '../files/cilin_hier_perword'
+outp1 = '../models/cb_cilin_def_palin_3.model'
+outp2 = '../modelscb_cilin_def_palin_3.vector'
+model = Word2Vec(LineSentence(inp), sg=0,size=300, window=3, min_count=0,\
+                     workers=multiprocessing.cpu_count(),\
+                     iter = 5)#adjust the parameters to get different models
+model.save(outp1)
+model.wv.save_word2vec_format(outp2, binary=False)
